73c1ad4edb850d4b9e9e4d0bfe121d0bffdbd427
asanalimukhtar <assanali.mukhtar@narxoz.kz>
2025-04-15 09:01:24 +0500

    Add Logger singleton for logging messages

---
+++ ChainofResponsibility/src/Logger.java
@@ -0,0 +1,11 @@
+public class Logger {
+    private static Logger instance = new Logger();
+    private Logger() { }
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
