5dbbef7e2aabb9629f33c0eb8e279e22fa3e3001
asanalimukhtar <assanali.mukhtar@narxoz.kz>
2025-04-15 13:19:58 +0500

    Add MacroCommand to group commands (Goodnight Mode)

---
+++ CommandPattern/src/MacroCommand.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MacroCommand implements Command {
+    private List<Command> commands = new ArrayList<>();
+
+    public void addCommand(Command command) {
+        commands.add(command);
+    }
+
+    @Override
+    public void execute() {
+        for (Command command : commands) {
+            command.execute();
+        }
+    }
+
+    @Override
+    public void undo() {
+        // Откат команд в обратном порядке
+        for (int i = commands.size() - 1; i >= 0; i--) {
+            commands.get(i).undo();
+        }
+    }
+}
