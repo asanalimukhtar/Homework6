809d1fd0b5af62e959c1ea4d916d8c188becb293
asanalimukhtar <assanali.mukhtar@narxoz.kz>
2025-04-15 09:00:53 +0500

    Add SupportDemo with test issues and chain configuration

---
+++ ChainofResponsibility/src/SupportDemo.java
@@ -0,0 +1,15 @@
+public class SupportDemo {
+    public static void main(String[] args) {
+        SupportHandler faq = new FAQBotHandler();
+        SupportHandler junior = new JuniorSupportHandler();
+        SupportHandler senior = new SeniorSupportHandler();
+
+        faq.set_next(junior).set_next(senior);
+
+        String[] issues = {"password_reset", "refund_request", "account_ban", "unknown_bug"};
+
+        for (String issue : issues) {
+            faq.handle(issue);
+        }
+    }
+}
